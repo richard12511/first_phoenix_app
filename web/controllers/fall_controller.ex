c469fd730a8b67dde27c9fc153dd38d3bf11335c
richard12511 <richardnew12511@gmail.com>
2018-10-26 18:55:22 -0500

    initial commit

---
+++ web/controllers/fall_controller.ex
@@ -0,0 +1,35 @@
+defmodule Fall.FallController do
+  use Fall.Web, :controller
+
+  def welcome(conn, _params) do
+    choices = ["Earth": 1, "Moon": 2, "Mars": 3]
+    render conn, "welcome.html", choices: choices
+  end
+
+  def faller(conn, %{"calculation" => %{"planemo" => planemo, "distance" => distance}}) do
+    calc_planemo = String.to_integer(planemo)
+    calc_distance = String.to_integer(distance)
+    speed = fall_velocity(calc_planemo, calc_distance)
+    choices = ["Earth": 1, "Moon": 2, "Mars": 3]
+
+    render conn, "faller.html", speed: speed, choices: choices
+  end
+
+  def fall_velocity(1, distance) do
+    :math.sqrt(2 * 9.8 * distance)
+  end
+
+  def fall_velocity(2, distance) do
+    :math.sqrt(2 * 1.6 * distance)
+  end
+
+  def fall_velocity(3, distance) do
+    :math.sqrt(2 * 3.71 * distance)
+  end
+  # def faller(conn, params) do
+  #   choices = ["Earth": 1, "Moon": 2, "Mars": 3]
+  #   speed = 0
+  #   render conn, "faller.html", speed: speed, choices: choices
+  # end
+
+end
